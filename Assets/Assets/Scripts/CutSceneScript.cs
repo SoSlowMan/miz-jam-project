37494f64a262f93ed922418e1c3ae4cc91615d90
konstantin <kosman.forgames@yandex.ru>
2020-08-20 15:46:46 +0300

    first starting anim

---
+++ Assets/Assets/Scripts/CutSceneScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneScript : MonoBehaviour
+{
+    public string currentLevel;
+    public string nextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().name;
+        switch (currentLevel)
+        {
+            case "StartCutScene":
+                nextLevel = "SampleScene";
+                break;
+            case "EndCutScene":
+                nextLevel = "EndingScreen";
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
